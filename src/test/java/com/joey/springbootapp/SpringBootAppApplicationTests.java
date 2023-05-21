401b26c96f982692d4fe1f772396be419eab5d3b
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-05-21 14:34:11 -0500

    initial commit

---
+++ src/test/java/com/joey/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
