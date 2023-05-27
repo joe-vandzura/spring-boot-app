28e1a59ab56156a224b205b9c426f29a1127ef14
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-05-26 23:29:48 -0500

    pushing changes. still need to fix breaks

--- src/main/java/com/joey/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/joey/springbootapp/SpringBootAppApplication.java
@@ -3,8 +3,10 @@ package com.joey.springbootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableJpaRepositories("com.joey.springbootapp.repositories")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
